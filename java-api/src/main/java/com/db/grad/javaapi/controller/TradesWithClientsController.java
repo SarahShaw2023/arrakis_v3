d57672e8f67cb53b5afae4a91ffac5f20fcba623
Sarah Shaw <sarah.y.shaw.db@gmail.com>
2023-08-07 15:38:38 +0100

    Added client info to trades instead of bonds

---
+++ java-api/src/main/java/com/db/grad/javaapi/controller/TradesWithClientsController.java
@@ -0,0 +1,26 @@
+package com.db.grad.javaapi.controller;
+
+import com.db.grad.javaapi.model.TradeWithClient;
+import com.db.grad.javaapi.service.TradesWithClientsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+public class TradesWithClientsController {
+    private TradesWithClientsService tradesWithClientsService;
+
+    @Autowired
+    public TradesWithClientsController(TradesWithClientsService bwc) {
+        tradesWithClientsService = bwc;
+    }
+
+    @GetMapping("/tradesWithClients")
+    public List<TradeWithClient> getBondsWithClients(){
+        return tradesWithClientsService.getAllBondsWithClients();
+    }
+}
