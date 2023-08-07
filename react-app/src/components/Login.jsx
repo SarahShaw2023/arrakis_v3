c9232e0bef4664cc5fee32785c6d19770481c8f5
Benjamin Linford <blinford@live.unc.edu>
2023-08-07 12:09:49 -0400

    added basic firebase auth

--- react-app/src/components/Login.jsx
+++ react-app/src/components/Login.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { login } from "../services/TradeServices";
 import { Link } from "react-router-dom";
 // import Planet from "../venus.png"
-import CryptLogic from "../utils/crypt";
 import {signInWithEmailAndPassword} from "firebase/auth";
 import auth from "../firebase"
 
@@ -16,42 +14,43 @@ const Login = ({userID, setUserID}) => {
     const navigate = useNavigate();
         
     const onButtonClick = () => {
-         signInWithEmailAndPassword(auth,email,password)
-                 .then((userCredential)=> {
-                     navigate("/Bonds");})
+         signInWithEmailAndPassword(auth,username,password)
+                 .then((user)=> {
+                    setUserID(user.uid);
+                    navigate("/home");})
                   .catch((error) => {
                  console.log(error)
                  })
         setUsernameError("")
         setPasswordError("")
 
-        if ("" === username) {
-            setUsernameError("please enter your username")
-            return
-        }
+        // if ("" === username) {
+        //     setUsernameError("please enter your username")
+        //     return
+        // }
 
-        if (!/^[A-Za-z0-9]*$/.test(username)) {
-            setUsernameError("invalid username")
-            return
-        }
+        // if (!/^[A-Za-z0-9]*$/.test(username)) {
+        //     setUsernameError("invalid username")
+        //     return
+        // }
 
-        if ("" === password) {
-            setPasswordError("please enter your password")
-            return
-        }
+        // if ("" === password) {
+        //     setPasswordError("please enter your password")
+        //     return
+        // }
 
-        //Encrypt password
-        let pwd = CryptLogic.encryptStr(password); //password
-        login(username, pwd)
-            .then(res => {
-                console.log("Login response: " + res.data);
-                setUserID(res.data);
-                navigate("/home")
-            })
-            .catch(err => {
-                console.log("Login error response: " + err);
-                setUserID(0);
-        })
+        // //Encrypt password
+        // let pwd = CryptLogic.encryptStr(password); //password
+        // login(username, pwd)
+        //     .then(res => {
+        //         console.log("Login response: " + res.data);
+        //         setUserID(res.data);
+        //         navigate("/home")
+        //     })
+        //     .catch(err => {
+        //         console.log("Login error response: " + err);
+        //         setUserID(0);
+        // })
     }
 
     return <div className={"mainContainer"}>
@@ -63,7 +62,7 @@ const Login = ({userID, setUserID}) => {
         <div className={"inputContainer"}>
             <input
                 value={username}
-                placeholder="username"
+                placeholder="email"
                 onChange={ev => setUsername(ev.target.value)}
                 className={"inputBox"} />
             <label className="errorLabel">{usernameError}</label>
@@ -88,16 +87,6 @@ const Login = ({userID, setUserID}) => {
         </div>
         {/* Reset password option */}
         <br />
-        <div className={"resetOption"}>
-            <Link to="/resetLogin">
-                Forgot Password?
-            </Link>
-        </div>
-        <div className={"resetOption"}>
-            <Link to="/createAccount">
-                Create New Account
-            </Link>
-        </div>
     </div>
 }
 
