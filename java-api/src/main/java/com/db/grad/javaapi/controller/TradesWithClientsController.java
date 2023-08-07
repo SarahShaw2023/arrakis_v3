aea36dc8e71a881436338010915403b94fc092b2
Sarah Shaw <sarah.y.shaw.db@gmail.com>
2023-08-07 16:23:36 +0100

    Added tradeWithClients with user based logic

--- java-api/src/main/java/com/db/grad/javaapi/controller/TradesWithClientsController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/TradesWithClientsController.java
@@ -5,6 +5,7 @@ import com.db.grad.javaapi.service.TradesWithClientsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,7 +21,12 @@ public class TradesWithClientsController {
     }
 
     @GetMapping("/tradesWithClients")
-    public List<TradeWithClient> getBondsWithClients(){
-        return tradesWithClientsService.getAllBondsWithClients();
+    public List<TradeWithClient> getTradesWithClients(){
+        return tradesWithClientsService.getAllTradesWithClients();
+    }
+
+    @GetMapping("/tradesWithClients/user")
+    public List<TradeWithClient> getTradesWithClientsByUser(@RequestParam("userID") int userID) {
+        return tradesWithClientsService.getTradesWithClientsByUserID(userID);
     }
 }
--- react-app/src/components/Home.jsx
+++ react-app/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getAllBonds, getAllBondsMaturity, getAllTrades } from '../services/TradeServices';
+import { getAllBonds, getAllBondsMaturity, getAllTrades, getAllTradesWithClients } from '../services/TradeServices';
 import { useNavigate } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css'
@@ -45,7 +45,8 @@ export const Home = ({userID, setUserID}) => {
         })
     }
     const getTradesFromAPI = ()=>{
-        getAllTrades(userID)
+        //getAllTrades(userID)
+        getAllTradesWithClients(userID)
             .then(res => {
                 console.log(res.data);
                 setTrades(res.data);
@@ -78,7 +79,8 @@ export const Home = ({userID, setUserID}) => {
         return <tr>
             <td>{data.book_id}</td>
             <td>{data.bond_id}</td>
-            <td>{data.counterparty_id}</td>
+            <td>{data.client_id}</td>
+            <td>{data.client_name}</td>
             <td>{data.quantity}</td>
             <td>{data.currency}</td>
             <td>{statusCheck(data.status)}</td>
@@ -131,7 +133,8 @@ export const Home = ({userID, setUserID}) => {
                 <tr>
                     <th>Book ID</th>
                     <th>Bond ID</th>
-                    <th>Counterparty ID</th>
+                    <th>Client ID</th>
+                    <th>Client Name</th>
                     <th>Quantity</th>
                     <th>Currency</th>
                     <th>Status</th>
